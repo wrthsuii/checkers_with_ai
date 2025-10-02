70df5d678bce4741b1bef1151af5404e697370eb
Vladyslava Tytarenko <vladislavaaa.022@gmail.com>
2025-10-02 12:43:58 +0300

    initial commit

---
+++ checkers-with-ai/checkers_with_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkers_with_aiApp.swift
+//  checkers-with-ai
+//
+//  Created by Владислава Титаренко on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct checkers_with_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
